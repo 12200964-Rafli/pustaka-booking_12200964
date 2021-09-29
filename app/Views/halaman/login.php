832c98d16002128f4c090782d1367a0aa800d143
12200964-rafli <kehidupanrafli@gmail.com>
2021-09-29 21:57:52 +0700

    tugas pertemuan 2

---
+++ app/Views/halaman/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perpustakaan Login</title>
+</head>
+<body>
+    <h1>Selamat datang di Sistem Informasi Perpustakaan</h1>
+    <p>Ini adalah layanan perpustakaan digital masa kini
+        <a href="<?=base_url(relativePath: '/daftar-member')?>">klik daftar</a> untuk menjadi member
+    </p>
+</body>
+</html>
